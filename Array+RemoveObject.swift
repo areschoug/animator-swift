4fa50bc51c56a0d9b68624538dc6c6cf16a21a44
areschoug <andreas.areschoug@gmail.com>
2014-11-24 09:26:07 +0100

    Addad needed Array+RemoveObject

---
+++ Array+RemoveObject.swift
@@ -0,0 +1,32 @@
+//
+//  Array+RemoveObject.swift
+//
+//  Created by areschoug on 20/11/14.
+//
+
+//This file should probably not be in this project
+
+import Foundation
+
+extension Array{
+
+    
+    mutating func removeObject<T: Equatable>(object: T) -> Bool{
+        var index: Int?
+        for (idx, objectToCompare) in enumerate(self) {
+            if let to = objectToCompare as? T {
+                if object == to {
+                    index = idx
+                }
+            }
+        }
+        
+        if(index != nil) {
+            self.removeAtIndex(index!)
+            return true
+        }
+        
+        return false
+    }
+    
+}
